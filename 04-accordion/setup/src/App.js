ce20b188e6dace215bb3dea11e98f7756832d2ea
chidera-ovuoba <107512707+chidera-ovuoba@users.noreply.github.com>
2022-07-07 16:26:38 +0100

    7/7/2022 commit

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
-}
+  const [state, setState] = useState(data);
+ 
+  return <>
+    <main>
+      <div className="container">
+        <h3>questions and answers about</h3>
+        <div>
+          {state.map((item)=> {
+            return <SingleQuestion {...item} key={ item.id} />
+        })}
+        </div>
+      </div>
+    </main>
+  
+  </>;
+} 
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  const [isClicked, setIsClicked] = useState(false);
+  return <>
+    <div className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={()=>setIsClicked((prev)=>!prev)}>{isClicked ? <AiOutlineMinus /> :<AiOutlinePlus/>}</button>  
+      </header>
+      <p>{isClicked ? info :''}</p>    
+ </div>
+  
+  </>;
 };
 
 export default Question;
