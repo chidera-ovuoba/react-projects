59b3e58f820e28b22370b3ee706855aafcf2ba39
chidera-ovuoba <107512707+chidera-ovuoba@users.noreply.github.com>
2022-07-09 15:39:40 +0100

    7/9/2022 commit

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,35 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+   const refContainer = useRef(null);
+   
+  const toggle = () => {
+    refContainer.current.classList.toggle('show-container');
+   }
+  return <nav>
+    <div className='nav-center'>
+    <div className="nav-header">
+      <img src={logo} alt="" className="logo" />
+        <FaBars className="nav-toggle" onClick={toggle} />
+        </div>
+      <div className="links-container" ref={refContainer} >
+      <div className="links">
+        {links.map((item) => {
+          const { id, text, url } = item;
+          return <a href={url} key={id}>{text}</a>
+      })}
+        </div>
+        </div>
+      <div className="social-icons">
+        {
+          social.map((item) => {
+             const { id,icon, url } = item;
+                return <a href={url} key={id}>{icon}</a>
+          })
+      }
+        </div>
+        </div>
+   </nav>
 }
-
+ 
 export default Navbar
