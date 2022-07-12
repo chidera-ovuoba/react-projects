fe41d75642222889ff0041e237c7437bb248f5cf
chidera-ovuoba <107512707+chidera-ovuoba@users.noreply.github.com>
2022-07-12 10:49:31 +0100

    7/12/2022 commit

--- 16-markdown-preview/setup/src/App.js
+++ 16-markdown-preview/setup/src/App.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [state, setState] = useState('# markdown Preview');
+  return <>
+    <div className="markdown">
+      <textarea className="input"
+        type='text'
+        value={state}
+        onChange = {(e)=>setState(e.target.value)}
+      ></textarea>
+      <div className="result"><ReactMarkdown>{state}</ReactMarkdown></div>
+    </div>
+  
+  
+  </>
 }
 
-export default App
+export default App 
